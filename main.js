6fd46e544548dd5fcdc043ec2bb20958d933cd04
ihsanfaisal <ihsanfaisal@gmail.com>
2024-02-12 09:08:21 +0700

    Memperbaiki fungsi hapus

--- main.js
+++ main.js
@@ -48,5 +48,5 @@ export async function tambahSiswa(val) {
 }
 
 export async function hapusSiswa(docId) {
-  await deleteDoc(doc(db, "siswa"), docId);
+  await deleteDoc(doc(db, "siswa", docId));
 }
